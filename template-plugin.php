6997206b94f7ee86c27efe1bf0353dba1a258649
crebow <42725472+crebow@users.noreply.github.com>
2018-09-14 11:35:51 +0900

    first up

---
+++ template-plugin.php
@@ -0,0 +1,15 @@
+<?php
+/*
+Plugin Name: Crebow Webhook Tester Plugin
+Description: Plug-in for testing Webhook reception.
+Version: 0.0.0
+Plugin URI: https://crebow.info/
+Author: Tomoya Matsumoto
+Author URI: https://crebow.info/
+License: GPLv2
+Text Domain: crw-webhook-tester
+Domain Path: /languages
+*/
+
+define( 'CRWWHTESTER_PATH', plugin_dir_path( __FILE__ ) );
+
